df8d1d09e9c24e525a609d6247c336c63f00cbdc
Timo Heister <timo.heister@gmail.com>
2018-04-07 16:02:43 -0400

    new data

--- changelog.py
+++ changelog.py
@@ -2,13 +2,21 @@ import re
 
 pattern = re.compile(r"\((.*) (\d{2,4}/\d{2}/\d{2}).*\)")
 
+def print_names(date, names):
+    #print "*", names
+    
+    xx = names.replace(" and ", ",").replace("& "," and ").replace(" 20",",20").split(",")
+    for x in xx:
+        name = x.strip()
+        if len(name)>0 and "20" not in name:
+            print date, name.lower()
+
+
+
 for i, line in enumerate(open('changelog.in')):
     for match in re.finditer(pattern, line):
         date = match.groups()[1].replace("/","-")
         #print "line:", line, "no:", i, " date:", date
         names = []
-        for name in match.groups()[0].split(","):
-            name = name.replace(",","").strip()
-            if len(name)>0:
-                print date, name
+        print_names(date, match.groups()[0])
 
--- cleanup.py
+++ cleanup.py
@@ -4,10 +4,35 @@
 authormapping = {"adamkosik":"adam kosik", 
 "third-party":"", 
 "msteigemann":"martin steigemann",
+                 "alex":"alex kokomov",
+                 "anne-glerum":"anne glerum",
+                 "asartori86":"alberto sartori",
+                 "barker29":"Andrew Barker",
+                 "benjamin":"benjamin brands",
+                 "darndt":"daniel arndt",
+                 "elddle":"eldar khattatov",
+                 "fsonner":"florian sonner",
+                 "givalz":"giovanni alzetta",
+                 "ikligge":"ingo kligge",
+                 "jaeryunyim":"jaeryun yim",
+                 "jason p. sheldon":"jason sheldon",
+                 "jwitte":"julius witte",
+                 "loylick":"alex kokomov",
+                 "mfraters":"menno fraters",
+                 "mike h":"mike harmon",
+                 "mike":"mike harmon",
+                 "nivesh":"nivesh dommaraju",
+                 "o-sutton":"oliver sutton",
+                 "rajat":"rajat arora",
+                 "rombur":"bruno turcksin",
+                 "sairajat":"rajat arora",
+                 "simfeld":"samuel imfeld",
+                 "vishalkenchan":"vishal boddu",
 "oneliefleft":"toby d. young",
 "tcclevenger":"conrad clevenger",
 "agrayver":"alexander grayver",
 "amola":"andrea mola",
+"abner j. salgado":"abner salgado",
 "baerbel jannsen":"baerbel janssen",
 "dakshinai":"dakshina ilangovan",
 "dakshina.ilangova":"dakshina ilangovan",
@@ -17,6 +42,7 @@ authormapping = {"adamkosik":"adam kosik",
 "oneliefleft":"toby d. young",
 "praveen c":"praveen chandrashekar",
 "cvs":"",
+"yy":"",
 "wb":"wolfgang bangerth",
 "rh":"Ralf Hartmann",
 "gk":"Guido Kanschat",
@@ -29,7 +55,7 @@ def make_name(name):
     if "@" in nameparts[-1]:
         name = " ".join(nameparts[0:-1])
     name = authormapping[name] if name in authormapping else name
-    if "2015" in name:
+    if "20" in name:
         return ""
     return name
 
@@ -38,7 +64,10 @@ def make_name(name):
 for line in fileinput.input():
     date,name = line.replace("\n","").split(" ",1)
     yearmonth = date[0:7]
-    name = make_name(name)
-    #print yearmonth, name
-    if len(name)>1:
-        print date,name
+    cleanname = make_name(name)
+    
+    if len(cleanname)<2:
+        continue
+    
+    #print "{} ({})".format(cleanname,name)   # for testing
+    print date,cleanname
